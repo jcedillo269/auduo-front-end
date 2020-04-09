e01e24c2eebbd26ca5052738c729e9c76b65519c
jcedillo269 <jcedillo269@gmail.com>
2020-04-09 01:02:36 -0500

    Added styling and campbell cs file

--- src/App.js
+++ src/App.js
@@ -4,15 +4,18 @@ import { Component } from 'react'
 import './App.css';
 import SpotifyWebApi from 'spotify-web-api-js';
 import queryString from 'query-string';
+//import Spotify from './spotify-player.js'
+//const Spotify = new SpotifyPLayback();
+import './campbell-code/style2.css' //Bring over styling
 const spotifyApi = new SpotifyWebApi();
 
 
-let SpotifyTextColor = '#1DB954'
+let SpotifyTextColor = '#1DB954' 
 let defaultStyle = {color: 'white'}
 let playMap = new Map() //map {key: playlistName, value: playlistID}
 
 
-class Input extends Component{
+class Input extends Component{ //component made by Maggie that calls the MusixMatch API to get lyrics 
   state = {
       trackTitle: '',
       trackArtist: '',
@@ -22,7 +25,7 @@ class Input extends Component{
 
   
 
-  findSong(name,artist) {
+  findSong(name,artist) { //function to get lyrics from song name and artist
     this.setState({
       trackTitle: name,
       trackArtist: artist
@@ -43,7 +46,7 @@ class Input extends Component{
     } 
   }
 
-  findTranslation = (e) => {
+  findTranslation = (e) => { //doesnt work right now
       //e.preventDefault();
       console.log('howdy')
       // e.preventDefault();
@@ -70,7 +73,7 @@ class Input extends Component{
       let song = this.props.songName
       let artist = this.props.artist
 
-      return(
+      return( //returns this HTML/JS code to the main App component defined below
           
               // {value => {
               //     return(
@@ -81,10 +84,6 @@ class Input extends Component{
                           <div>
                             <button
                               onClick = {() => {
-                                // this.setState({
-                                //   trackTitle: song,
-                                //   trackArtist: artist
-                                // });
                                 this.findSong(song,artist);
                               }
                             } 
@@ -92,67 +91,13 @@ class Input extends Component{
                               Get Lyrics
                             </button>
                           </div>
-                          {/* <form onSubmit={this.findSong}>
-                              <div className="form-group">
-                                  <input 
-                                      type="text" 
-                                      className="form-control form-control-lg" 
-                                      placeholder="Song Title" 
-                                      name="trackTitle" 
-                                      value={this.state.trackTitle}
-                                      onChange={this.onChangeT.bind(this)}
-                                  />
-                              </div>
-                              <div className="form-group">
-                                  <input 
-                                      type="text" 
-                                      className="form-control form-control-lg" 
-                                      placeholder="Song Artist" 
-                                      name="trackArtist" 
-                                      value={this.state.trackArtist}
-                                      onChange={this.onChangeA.bind(this)}
-                                  />
-                              </div>
-                              <button className="btn btn-primary btn-lg btn-block mb-5" type="submit">Submit</button>
-                          </form> */}
-                          {/* <form onSubmit={this.findTranslation}>
-                              <div className="form-group">
-                                  <input 
-                                      type="text" 
-                                      className="form-control form-control-lg" 
-                                      placeholder="Song Title" 
-                                      name="trackTitle" 
-                                      value={this.state.trackTitle}
-                                      onChange={this.onChangeT.bind(this)}
-                                  />
-                              </div>
-                              <div className="form-group">
-                                  <input 
-                                      type="text" 
-                                      className="form-control form-control-lg" 
-                                      placeholder="Song Artist" 
-                                      name="trackArtist" 
-                                      value={this.state.trackArtist}
-                                      onChange={this.onChangeA.bind(this)}
-                                  />
-                              </div>
-                              <div className="form-group">
-                                  <input 
-                                      type="text" 
-                                      className="form-control form-control-lg" 
-                                      placeholder="Translation Language" 
-                                      name="Language" 
-                                      value={this.state.Language}
-                                      onChange={this.onChangeL.bind(this)}
-                                  />
-                              </div>
-                              <button className="btn btn-primary btn-lg btn-block mb-5" type="submit">Translate</button>
-                          </form> */}
+                         
+
                           {
                             this.state.lyrics &&
                             <div>
                               <h3 style={{'color':'white'}}>Lyrics:</h3>
-                            <p style={{color: SpotifyTextColor}}>{this.state.lyrics}</p>
+                            <p style={{color: SpotifyTextColor }}>{this.state.lyrics}</p> {/* prints out lyrics in paragraph style */}
                             </div>
                           }
                       </div>
@@ -163,71 +108,87 @@ class Input extends Component{
 
 
 
-class Playlist extends Component { //practice component that I didn't end up using 
-  render() { //you can add html and JS code in the render () then when you include the component as a tag in the App component it adds it on to there
-    let playlist = this.props.playlist
+class Playlist extends Component {
+  render() {
+    let playlist = this.props.playlist;
     return (
-      <div style={{...defaultStyle, display: 'inline-block', width: "25%"}}>
-        <img src={playlist.imageUrl} style={{width: '100px'}}/>
-        <h3>{playlist.name}</h3>
-        <ul>
-          {playlist.songs.map(song => 
-            <li>{song.name}</li>
+      <div>
+      {playlist ?
+        <div>
+        <div style={{...defaultStyle, display: 'inline-block', width: "25%"}}> {/* In line styling*/}
+        <img src={playlist.imageUrl} style={{width: '100px'}}/> {/* playlist.imageUrl gets you access to the picture of the playlist */}
+        <h3>{playlist.name}</h3>      {/*gets you access to input playlist name*/}
+        <p>Size: {playlist.songs.length}</p> {/*access to song length */}
+        <ul style={{listStyle:'none'}}>
+          {playlist.songs.map(song =>  // map is like a forEach function. So for every song in the playlist
+
+              <li><button onClick= {() => this.props.getArtist(song.name,song.id)}>{song.name} </button></li> //list of buttons for all the song names
+
           )}
         </ul>
       </div>
+      </div>
+      :
+      <div>
+      <h3>Select a Playlist from the left to get started.</h3>
+      </div>
+    }
+    </div>
     );
   }
 }
+
+
+
 
 
 let songMap = new Map() //map: {key: songName, value: id}
 class App extends Component { //main component
   constructor(){
     super();
-    this.state = {
+    this.getSongArtist = this.getSongArtist.bind(this); //Allows getSongArtist function to be called in other components
+    this.state = { //states are like member variables in Java classes. Whenever a state is updated the whole web page re-redners
       loggedIn: false,
       gotSong: false,
       selectedSong: '',
       selectedSongArtist: '',
+      selectedExplicit: false,
       songMap: new Map(),
       token: 'null',
       userInfo: {name: 'Stranger', img: 'https://abs.twimg.com/sticky/default_profile_images/default_profile_normal.png'},
-      playlists: '' //empty playlists array?,
-      
+      playlists: '', //empty playlists array?,
+      selectedPlaylist: ''
     }
   }
 
   componentDidMount(){ //this is where I connect to the API and make the requests for userInfo, playlists, and songs in playlist
     let result = queryString.parse(window.location.search) 
-    //console.log('Token in didMount: ', result.access_token)
     let accessToken = result.access_token //access token that we use to communicate to spotify API
     if(accessToken){ //if we have a valid access token
       spotifyApi.setAccessToken(accessToken) 
       this.setState( //states are like private variables in a C++/Java class
         {
           loggedIn: true, //set logged in condition to true
-          token: accessToken 
+          token: accessToken  //save access token in state.token
         }
       )
 
       this.getUserInfo() //function call. Function defined below
       this.getUserPlaylists(accessToken) //function call. Function defined below
-      //this.songMap.set('Hello',"hi")
     }
   }
 
 
-  getSongArtist(name,id){
+  getSongArtist(name,id){ //function to retreive song artist given song name and song id
     //console.log('Ran get song artist nmae',id)
-    let x = null
     spotifyApi.getTrack(id)
     .then((response) => {
       //console.log(response)
       this.setState(
         {
           selectedSong: name,
-          selectedSongArtist: response.artists[0].name
+          selectedSongArtist: response.artists[0].name, //response.artists returns an array of arists but we only get the first one
+          selectedExplicit: response.explicit
         }
       )
     }
@@ -245,12 +206,10 @@ class App extends Component { //main component
         <ul style={{listStyle:'none'}}>
           {playlist.songs.map(song =>  // map is like a forEach function. So for every song in the playlist
 
-              //<li><button onclick="getSongArtist('song.name','song.id')">{song.name}</button></li> //list of all the song names
-              <li><button onClick= {() => this.getSongArtist(song.name,song.id)}>{song.name} </button></li> //list of all the song names
+              <li><button onClick= {() => this.getSongArtist(song.name,song.id)}>{song.name} </button></li> //list of buttons for all the song names
 
           )}
         </ul>
-        {/* {this.getSongArtist('5GUYJTQap5F3RDQiCOJhrS')} */}
       </div>
       );
     
@@ -292,10 +251,10 @@ class App extends Component { //main component
     .then(playlists => this.setState({
       playlists: playlists.map(item => {
         return {
-          name: item.name,
-          imageUrl: item.images[0].url, 
-          id: item.id,
-          songs: item.trackDatas
+          name: item.name, //sets playlist name
+          imageUrl: item.images[0].url, //sets playlist image 
+          id: item.id, //sets playlist id
+          songs: item.trackDatas //array of songs
         }
     })
     }))
@@ -312,7 +271,7 @@ class App extends Component { //main component
       this.setState(
         {
           userInfo:{
-            name: response.display_name,
+            name: response.display_name, //sets Spotifu user name
             img: response.images[0] ?  response.images[0].url : 'https://abs.twimg.com/sticky/default_profile_images/default_profile_normal.png' //if user does not have profile picture, use default
           }
         }
@@ -320,8 +279,32 @@ class App extends Component { //main component
     })
   }
 
+  printPlaylistButton(playlist){
+    return(
+      <button onClick= {() => this.changeSelectedPlaylist(playlist.id)}>{playlist.name}</button>
+    );
+  }
 
-  getSongId(){
+  getPlaylistFromId(id){
+    let result = null;
+    this.state.playlists.map(playlist =>
+      {
+        if(playlist.id == id) result = playlist;
+
+      })
+    return result;
+  }
+
+  changeSelectedPlaylist(id){
+    this.setState(
+      {
+        selectedPlaylist: id
+      }
+    )
+  }
+
+
+  getSongId(){ //function I never used ***remove later****
     this.state.playlists.map(playlist => {
       playlist.songs.map(song => 
         songMap.set(song.id,song.name)
@@ -329,15 +312,9 @@ class App extends Component { //main component
     }
   
     )
-    // this.setState(
-    //   {
-    //     gotSong: true
-    //   }
-    // )
   }
 
   render() { //this is the main part of the html code that gets displayed to the screen
-    /*let name = 'Jacob' */
     let customeStyle = { color: '#61dbfb', fontSize: '100px', display: 'inline-block'} //defines a custome style that we can use for in-line styling
     return (
       <div className="App">
@@ -346,49 +323,102 @@ class App extends Component { //main component
         {this.state.loggedIn ? //so this is like an if statement. If the user is logged in print all code until line 159
           <div style={{paddingBottom: '75px'}}> {/*whenever you use the if statement thing you need to include a div around like the true part*/}
             {/* <p style={{color:'white'}}>Logged In!</p>  */}
-            {this.state.playlists && //this is another if statement that checks if playlists have informations 
-              <div>
-                {/* <p style={{color: 'white', fontSize: '25px'}}>Got users playlists _ test</p>  */}
-                
-                {/* {
-                  this.state.playlists.map(playlist => {
-                      playlist.songs.map(song => 
-                        songMap.set(song.id,song.name)
-                      )
-                    }
-                  )
-                  
-                }  */}
 
-                <div>
-                {
-                  this.state.playlists.map(playlist => 
-                      this.printPlaylistInfo(playlist)
-                      
-                  )
-                }
+            <div class="split">
+              <div class="centered">
+                <div id="div1">
+                  <p id="title_text"> Select Playlist </p>
                 </div>
+
+                <div class="container">
+                    
+                  
+                </div>
+
+              </div>
+
+            </div>
+
+
+            {this.state.playlists && //this is another if statement that checks if playlists have informations 
+
+              <div>
+              <div class="split">
+                <div class="centered">
+                  <div id="div1">
+                    <p id="title_text"> Select Playlist </p>
+                  </div>
+
+                  <div class="container">
+                      {this.state.playlists.map(playlist =>
+                        this.printPlaylistButton(playlist)
+                      )}
+                    
+                  </div>
+
+                </div>
+              </div>
+              <Playlist playlist={this.getPlaylistFromId(this.state.selectedPlaylist)} getArtist={this.getSongArtist}/>  
                 <div>
                 <h3 style = {{fontSize: '50px', 'color': 'white'}}>Selected Song and Artist</h3>
-                <p style = {{fontSize: '25px', 'color': 'white'}}>{this.state.selectedSong} , {this.state.selectedSongArtist}</p>
+                      <p style = {{fontSize: '25px', 'color': 'white'}}>{this.state.selectedSong} , {this.state.selectedSongArtist}</p>
                 </div>
-              
+                {
+                  this.state.selectedExplicit && 
+                  <p>Warning: Explicit Lyrics</p>
+                }
+
                 
               </div>
+             
+
             }
-            
-            {/* let userChoice = {
-              {
-              songName: this.state.selectedSong,
-              artist: this.state.selectedSongArtist
-              }
-            } */}
-          <Input {...{songName: this.state.selectedSong, artist: this.state.selectedSongArtist}}/>
+
+            {/* <html>
+            <script src="https://sdk.scdn.co/spotify-player.js"></script>
+    <script>
+      {
+        window.onSpotifyWebPlaybackSDKReady = () => {
+          //const token = {this.state.token}//'BQC_O5UVfqzL8Sm7bbkkyxHwJGuSqUMz1WUj1JEkUGackpUXu7nJoljvAatkjSQjsUsyDh0c_6SUtLP6kj2ghr5Wvw_26oqaMnTedIT-FAEo2UkOpLmO2ypRJDtnE85BcWcEmZ1_NCbYkus2Ka4CKezHQ6tH4MOyZaKtV44rB_BFwmX9wD0';
+          const player = new Spotify.Player({
+            name: 'Web Playback SDK Quick Start Player',
+            getOAuthToken: cb => { cb(this.state.token); }
+          });
+    
+          // Error handling
+          player.addListener('initialization_error', ({ message }) => { console.error(message); });
+          player.addListener('authentication_error', ({ message }) => { console.error(message); });
+          player.addListener('account_error', ({ message }) => { console.error(message); });
+          player.addListener('playback_error', ({ message }) => { console.error(message); });
+    
+          // Playback status updates
+          player.addListener('player_state_changed', state => { console.log(state); });
+    
+          // Ready
+          player.addListener('ready', ({ device_id }) => {
+            console.log('Ready with Device ID', device_id);
+          });
+    
+          // Not Ready
+          player.addListener('not_ready', ({ device_id }) => {
+            console.log('Device ID has gone offline', device_id);
+          });
+          
+          // Connect to the player!
+          player.connect();
+        }
+      }
+      </script>
+
+            </html> */}
+
+
+          <Input {...{songName: this.state.selectedSong, artist: this.state.selectedSongArtist}}/> {/* calls the Input component defined above. Passes in props to the input component */}
           </div>
           : //else if the user is not logged in print the the code below
           <div>
-            <button
-              onClick = {() => window.location = 'https://auduo-backend.herokuapp.com/login'} 
+            <button //spotify login button
+              onClick = {() => window.location = 'http://localhost:8888/login'} 
               style = {{padding: '20px', 'margin-top': '20px', background:SpotifyTextColor, color:'white', fontSize: '20px' }}> 
               Sign in with Spotify
             </button>
